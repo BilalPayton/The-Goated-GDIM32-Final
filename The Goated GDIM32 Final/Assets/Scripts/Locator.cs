77f84650e2c92b77e2376df5240f2850f4f91806
Laura27Apr <leyangl4@uci.edu>
2026-02-17 18:55:31 -0800

    Laura

---
+++ The Goated GDIM32 Final/Assets/Scripts/Locator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Locator : MonoBehaviour
+{
+    public static Locator Instance { get; private set; }
+    public Player Player { get; private set; }
+    //public GameController GameController { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+
+        Instance = this;
+
+        GameObject playerObj = GameObject.FindWithTag("Player");
+        Player = playerObj.GetComponent<Player>();
+    }
+
+}
