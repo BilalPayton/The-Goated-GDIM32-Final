be9736c5ea1685a1ee991113a8f0977257c1cb5d
Laura27Apr <leyangl4@uci.edu>
2026-02-22 22:34:04 -0800

    Laura

--- The Goated GDIM32 Final/Assets/Scripts/GameController.cs
+++ The Goated GDIM32 Final/Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//Laura
 public enum GameState
 {
     Quest1,
@@ -8,11 +9,20 @@ public enum GameState
     Escaping
 }
 
+
 public class GameController : MonoBehaviour
 {
     public static GameController instance;
 
     public GameState CurrentState;
+
+    [SerializeField] private GameObject _zombiePrefab;
+    [SerializeField] private Transform[] _spawnPoints;
+    [SerializeField] private float _spawnInterval = 5f;
+    [SerializeField] private int _maxZombies = 10;
+
+    private int _currentZombieCount;
+    private float _spawnTimer;
 
     private void Awake()
     {
@@ -24,5 +34,45 @@ public class GameController : MonoBehaviour
        CurrentState = GameState.Quest1;
     }
 
-    
+    private void Update()
+    {
+        HandleSpawning();
+    }
+
+    private void HandleSpawning()
+    {
+ 
+        if (CurrentState == GameState.Quest2)
+        {
+            _spawnTimer += Time.deltaTime;
+
+            if (_spawnTimer >= _spawnInterval)
+            {
+                _spawnTimer = 0f;
+
+                if (_currentZombieCount < _maxZombies)
+                {
+                    SpawnZombie();
+                }
+            }
+        }
+    }
+
+    private void SpawnZombie()
+    {
+        if (_spawnPoints.Length == 0) return;
+
+        int index = Random.Range(0, _spawnPoints.Length);
+        Transform spawnPoint = _spawnPoints[index];
+
+        Instantiate(_zombiePrefab, spawnPoint.position, Quaternion.identity);
+
+        _currentZombieCount++;
+    }
+
+    public void ZombieDied()
+    {
+        _currentZombieCount--;
+    }
+
 }
