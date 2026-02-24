06811db9f42b5af8ecafbaa10979a3e4ba41501b
sloan05 <musicmonster127@gmail.com>
2026-02-24 15:18:16 -0800

    Player Movement Code + some terrain edits

--- The Goated GDIM32 Final/Assets/Scripts/Player.cs
+++ The Goated GDIM32 Final/Assets/Scripts/Player.cs
@@ -3,7 +3,82 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float _speed = 1.0f;
     
+
+    [SerializeField] public float _jumpVelocity = 3f;
+
+    [SerializeField] private Rigidbody _rb;
+
+    [SerializeField] Transform cameraTransform;
+
+    private bool _isGrounded;
+
+    [SerializeField] float sensitivity = 150f;
+
+    float xRotation;
+    float yRotation;
+
+   
+
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * _speed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * _speed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * _speed * Time.deltaTime);
+        }
+        if(Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
+            _rb.velocity = new Vector3 (_rb.velocity.x, _jumpVelocity, _rb.velocity.z);
+        }
+
+        xRotation -= Input.GetAxis("Mouse Y") * sensitivity;
+    yRotation += Input.GetAxis("Mouse X") * sensitivity;
+
+    xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+    transform.localEulerAngles = new Vector3(xRotation, yRotation, 0);
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded = false;
+        }
+    }
+
+
+    
+
+
+
+
+
+
+
+
 
 }
 
