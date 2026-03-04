f9e61ea9182c8eaf05a4ea209702ff37dbcb7a28
BilalPayton <supersmashorange@gmail.com>
2026-03-03 19:50:22 -0800

    Created and started coding NPCMultipleBranches class

--- The Goated GDIM32 Final/Assets/Scripts/NPCDialogue/NPCMultipleBranches.cs
+++ The Goated GDIM32 Final/Assets/Scripts/NPCDialogue/NPCMultipleBranches.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class NPCMultipleBranches : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _interactionDistance = 2.0f;
+    [SerializeField] private NPCDialogue _startNode;
+
+    private NPCDialogue _currentNode;
+
+    [SerializeField] private TextMeshPro _interactText;
+
+    private int _currentLine = 0;
+    private bool _runningDialogue;
+    private bool _waitingForPlayerResponse;
+
+    private void Start()
     {
-        
+        _currentNode = _startNode;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Player.Instance == null) return;
+
+        if(Vector3.Distance(transform.position, Player.Instance.transform.position) < _interactionDistance)
+        {
+            _interactText.gameObject.SetActive(true);
+
+            if(!_waitingForPlayerResponse && Input.GetKeyDown(KeyCode.Space))
+            {
+                
+            }
+            else if(!_runningDialogue)
+            {
+                
+            }
+        }
+        else
+        {
+            
+        }
     }
 }
