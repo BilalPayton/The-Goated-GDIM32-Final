78d175340d84a219702d725579f225c72f3aa014
BilalPayton <supersmashorange@gmail.com>
2026-03-06 23:58:26 -0800

    Continued making Joe's Dialogue

--- The Goated GDIM32 Final/Assets/Scripts/NPCDialogue/NPCMultipleBranches.cs
+++ The Goated GDIM32 Final/Assets/Scripts/NPCDialogue/NPCMultipleBranches.cs
@@ -10,7 +10,14 @@ public class NPCMultipleBranches : MonoBehaviour
 
     private NPCDialogue _currentNode;
 
-    [SerializeField] private TextMeshPro _interactText;
+    [SerializeField] private GameObject _interactText;
+    [SerializeField] private GameObject _npcDialogue;
+    // [SerializeField] private GameObject _playerReplyOptions;
+
+    [SerializeField] private GameObject _playerReply1;
+    [SerializeField] private GameObject _playerReply2;
+    [SerializeField] private GameObject _playerReply3;
+
 
     private int _currentLine = 0;
     private bool _runningDialogue;
@@ -29,9 +36,9 @@ public class NPCMultipleBranches : MonoBehaviour
         {
             _interactText.gameObject.SetActive(true);
 
-            if(!_waitingForPlayerResponse && Input.GetKeyDown(KeyCode.Space))
+            if(!_waitingForPlayerResponse && Input.GetKeyDown(KeyCode.Mouse0))
             {
-                
+                AdvanceDialogue();
             }
             else if(!_runningDialogue)
             {
@@ -40,7 +47,97 @@ public class NPCMultipleBranches : MonoBehaviour
         }
         else
         {
-            
+            EndDialogue();
         }
+    }
+
+    private void AdvanceDialogue()
+    {
+        _runningDialogue = true;
+        _interactText.gameObject.transform.position = new Vector3(-1, -1, -1);
+
+        if (_currentLine < _currentNode._lines.Length)
+        {
+            // if we still have NPC lines left, keep playing NPC lines
+            ShowNPCDialogue();
+
+            _currentLine++;
+        }
+        else if (_currentNode._playerReplyOptions != null && _currentNode._playerReplyOptions.Length > 0)
+        {
+            // show player dialogue options, if there are any
+            HideNPCDialogue();
+            _waitingForPlayerResponse = true;
+            ShowPlayerReplies();
+
+        }
+        else
+        {
+            // if there are no NPC or player lines left, close dialogue UI
+            EndDialogue();
+        }
+    }
+
+    private void EndDialogue()
+    {
+        _runningDialogue = false;
+        _waitingForPlayerResponse = false;
+        _currentNode = _startNode;
+        _currentLine = 0;
+        _interactText.gameObject.SetActive(false);
+    }
+
+    public void SelectedOption(int option)
+    {
+        _currentLine = 0;
+        _waitingForPlayerResponse = false;
+        HidePlayerReplies();
+
+        _currentNode = _currentNode._npcReplies[option];
+        AdvanceDialogue();
+    }
+
+
+    private void ShowNPCDialogue()
+    {
+        _npcDialogue.GetComponent<TMP_Text>().text = _currentNode._lines[_currentLine].ToString();
+        _npcDialogue.gameObject.SetActive(true);
+    }
+
+    private void HideNPCDialogue()
+    {
+        _npcDialogue.gameObject.SetActive(false);
+    }
+
+    private void ShowPlayerReplies()
+    {
+        for (int i = 0; i < _currentNode._playerReplyOptions.Length; i++)
+        {
+            if (i == 0)
+            {
+                _playerReply1.GetComponent<TMP_Text>().text = _currentNode._playerReplyOptions[i].ToString();
+            }
+
+            if (i == 1)
+            {
+                _playerReply2.GetComponent<TMP_Text>().text = _currentNode._playerReplyOptions[i].ToString();
+            }
+
+            if (i == 2)
+            {
+                _playerReply3.GetComponent<TMP_Text>().text = _currentNode._playerReplyOptions[i].ToString();
+            }
+        }
+
+        _playerReply1.gameObject.SetActive(true);
+        _playerReply2.gameObject.SetActive(true);
+        _playerReply3.gameObject.SetActive(true);
+    }
+
+    private void HidePlayerReplies()
+    {
+        _playerReply1.gameObject.SetActive(false);
+        _playerReply2.gameObject.SetActive(false);
+        _playerReply3.gameObject.SetActive(false);
     }
 }
