8c35f49479803069e766e5efa81f2b6a4154f2af
Laura27Apr <leyangl4@uci.edu>
2026-03-15 00:50:58 -0700

    Car Movement

---
+++ The Goated GDIM32 Final/Assets/Scripts/CarInteraction.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CarInteraction : MonoBehaviour
+{
+    private CarMovement movement;
+
+    private void Start()
+    {
+        movement = GetComponentInParent<CarMovement>();
+    }
+
+    private void OnMouseOver()
+    {
+        Debug.Log("Click to escape");
+    }
+
+    private void OnMouseDown()
+    {
+        if (movement != null)
+        {
+            movement.StartEscape();
+        }
+    }
+
+}
